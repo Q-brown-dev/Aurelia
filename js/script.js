0a6c874ff6807323ea492b2d60ddc6bc0e07cfea
unknown <anyikwabruno1@.com>
2026-07-27 14:04:29 -0700

    js/script.js folder & file

--- js/script.js
+++ js/script.js
@@ -147,3 +147,27 @@ buttons.forEach(button => {
     });
 
 });
+
+
+// ==========================
+// BACK TO TOP 
+//=============
+const btn = document.getElementById("backToTop");
+
+window.onscroll = function () {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        btn.style.display = "block";
+    }else {
+        btn.style.display = "none";
+    }
+}
+
+
+//Scroll to tp when clicked
+
+btn.onclick = function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
